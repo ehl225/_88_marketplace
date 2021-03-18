72232323d3b89dbaaf8bf69011585d9050641483
ehl225 <47624324+ehl225@users.noreply.github.com>
2021-03-18 14:38:23 -0400

    Update AboutUs.js

--- src/views/AboutUs.js
+++ src/views/AboutUs.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {address, email, phone, hours} from '../App.js';
 const parking = "Lot and street parking are available for our guests";
 const about = "We are Chicago's largest Chinese Supermarket! At 88 Marketplace, we strive to nourish and enrich local and interstate communities by providing fresh and high-quality products at a great value, exceptional service, and committing ourselves in fostering the growth of our employees and communities.";
-//const googleMapLink = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2971.810062144788!2d-87.6415806!3d41.8539149!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880e2c5f1a646fbd%3A0x2cd8e0115b911ce4!2s2105%20S%20Jefferson%20St%2C%20Chicago%2C%20IL%2060616!5e0!3m2!1sen!2sus!4v1609863887796!5m2!1sen!2sus";
 
 class AboutUs extends React.Component {
     render() {
